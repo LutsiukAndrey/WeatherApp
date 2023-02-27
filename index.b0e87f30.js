b19bd3c0ef6974ce80f5c56b52d0454160a994f5
LutsiukAndrey <107543302+LutsiukAndrey@users.noreply.github.com>
2023-02-27 13:03:31 +0000

    Deploying to gh-pages from @ LutsiukAndrey/WeatherApp@49faa7768d962995fec89df51f785227fd8b14c5 🚀

--- index.b0e87f30.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form").addEventListener("submit",(function(e){e.preventDefault();var o=e.target.query.value;console.log(o)})),console.log("sadfasdf");
-//# sourceMappingURL=index.b0e87f30.js.map
--- index.b2fc429b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form").addEventListener("submit",(e=>{e.preventDefault();const{value:o}=e.target.query;console.log(o)})),console.log("sadfasdf");
-//# sourceMappingURL=index.b2fc429b.js.map
