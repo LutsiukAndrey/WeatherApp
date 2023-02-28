292021d828a3dddb5729fc817530eaf9549c2f21
Andrii <lutsiukandrey1@gmail.com>
2023-02-28 22:10:33 +0200

    fix

--- src/js/fiveDaysMain.js
+++ src/js/fiveDaysMain.js
@@ -1,10 +1,10 @@
-import { fetchBcgImg } from './changeBcg';
-import { keys } from './keys';
-const { targetCityKey } = keys;
-const input = document.querySelector('.search-form');
-input.addEventListener('submit', onSubmit);
-const targetCity = localStorage.getItem(targetCityKey);
+// import { fetchBcgImg } from './changeBcg';
+// import { keys } from './keys';
+// const { targetCityKey } = keys;
+// const input = document.querySelector('.search-form');
+// input.addEventListener('submit', onSubmit);
+// const targetCity = localStorage.getItem(targetCityKey);
 
-if (targetCity) {
-  fetchBcgImg(targetCity);
-}
+// if (targetCity) {
+//   fetchBcgImg(targetCity);
+// }
