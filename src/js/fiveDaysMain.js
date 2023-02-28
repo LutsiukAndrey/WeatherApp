afafb70db052afda42adc0f6617248a889953760
Andrii <lutsiukandrey1@gmail.com>
2023-02-28 23:09:38 +0200

    fix

--- src/js/fiveDaysMain.js
+++ src/js/fiveDaysMain.js
@@ -1,8 +1,8 @@
-// import { fetchBcgImg } from './changeBcg';
-// import { keys } from './keys';
-// const { targetCityKey } = keys;
-// const input = document.querySelector('.search-form');
-// input.addEventListener('submit', onSubmit);
+import { fetchBcgImg } from './changeBcg';
+import { keys } from './keys';
+import '../js/header';
+const { targetCityKey } = keys;
+
 // const targetCity = localStorage.getItem(targetCityKey);
 
 // if (targetCity) {
